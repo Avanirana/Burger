965ff561ae6a1d565e1d763c9d32629944e449ca
Avanirana <avanirana33@gmail.com>
2019-07-16 15:54:23 -0500

    addedfile

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beef double cheese Burger',false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Happy meal Burger', false);
+
+
+
+
